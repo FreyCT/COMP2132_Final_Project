c0c78f6dc7731f26788bf5dc9bd9fcb9fb9ba638
PanderIT <FreydenIT@gmail.com>
2021-03-29 01:55:52 -0700

    Update hangman.js

--- scripts/hangman.js
+++ scripts/hangman.js
@@ -1,14 +1,9 @@
 document.body.style.backgroundImage = "url('images/hangman/black.jpg')";
 
 let words;
-// let word;
-// let wordLength;
-// let hint;
-// let correctGuesses;
 let game;
 let imagehandler;
 let imageNumber             = 0;
-// let incorrectGuesses        = 0;
 let hangmanImage            = document.getElementById("hangman-image");
 let wordDisplay             = document.getElementById("wordDisplay");
 let buttonsDisplay          = document.getElementById("alphabet");
@@ -95,7 +90,7 @@ class HangmanGame{
 
     draw() {
         this.incorrectGuesses++;
-        clearInterval(imagehandler);                                                                    //Needs attention
+        // clearInterval(imagehandler);
         if (this.incorrectGuesses < 8) {
             guessDisplay.innerHTML = `<p><strong>Guesses: </strong>${this.incorrectGuesses}/7</p>`;
             if (this.incorrectGuesses == 7) {
@@ -103,10 +98,11 @@ class HangmanGame{
                     this.gameOver("YOU LOSE");
                 }, 2000);
             }
-            imagehandler = setInterval(() => {
+            if(this.incorrectGuesses < 1) imagehandler = setInterval(() => {
                 imageNumber++;
                 hangmanImage.src = `images/hangman/hangman-0${this.incorrectGuesses}-${imageNumber%3}.png`;
             }, 150);
+            
         }
     }
 
@@ -127,108 +123,10 @@ fetch("json/words.json").then(function( response ){
         //forward the data to the next .then() stage
         return response.json();
     }
-})
-.then(function(data){                 
+}).then(function(data){                 
     words = data;
     newGame();
-})
-.catch(function(){
+
+}).catch(function(){
     console.log("fetch error");
 });
-
-//Create guess tracker
-// function guess(element) {
-//     let letter = element.textContent;
-//     element.disabled = true;
-//     element.hidden = true;
-//     let guessedWrong = true;
-//     for(let i in word) {
-//         if(word[i] == letter) {
-//             document.getElementById(`B${i}`).innerHTML = letter;
-//             correctGuesses++;
-//             guessedWrong = false;
-//         }
-//     }
-//     if(correctGuesses == wordLength) gameOver("YOU WIN");
-//     if(guessedWrong) hangman();
-// }
-
-//Create hang animation
-// function hangman() {
-//     incorrectGuesses++;
-//     clearInterval(imagehandler);
-//     if (incorrectGuesses < 8) {
-//         guessDisplay.innerHTML = `<p><strong>Guesses: </strong>${incorrectGuesses}/7</p>`;
-//         if (incorrectGuesses == 7) {
-//             setTimeout(() => {
-//                 gameOver("YOU LOSE");
-//             }, 2000);
-//         }
-//         imagehandler = setInterval(() => {
-//             imageNumber++;
-//             hangmanImage.src = `images/hangman/hangman-0${incorrectGuesses}-${imageNumber%3}.png`;
-//         }, 150);
-//     }
-// }
-
-// function gameOver(message) {
-//     clearInterval(imagehandler);
-//     wordDisplay.innerHTML = "";
-//     guessDisplay.innerHTML = `<p><strong>${message}</strong></p>`;
-//     hintDisplay.innerHTML = `<P onclick = "createGame()"><strong>PLAY AGAIN</strong></p>`;
-//     buttonsDisplay.innerHTML = "";
-// }
-
-// //Create Hint
-// function createHint() {
-//     hintDisplay.innerHTML = `<p>Hint: ${hint}</p>`;
-// }
-
-// //populate create guess work
-// function createBlanks() {
-//     for(let i in word) {
-//         let element = document.createElement("h3");
-//         element.innerHTML = (word[i] != "-") ? "_" : "-";
-//         element.setAttribute("id", `B${i}`);
-//         element.setAttribute("name", word[i]);
-//         wordDisplay.appendChild(element);
-//         console.log(word[i]);
-//     }
-// }
-
-// //Create and add button
-// function addButton(letter) {
-//     //Create an input type dynamically.
-//     let element = document.createElement("button");
-//     //Assign different attributes to the element.
-//     element.textContent = letter;
-//     element.setAttribute("id", letter);
-//     element.setAttribute("value", letter);
-//     element.setAttribute("type", "button");
-//     element.setAttribute("name", letter);
-//     element.setAttribute("onclick", "guess(this)");
-//     //Append the element in page (in span).
-//     buttonsDisplay.appendChild(element);
-// }
-
-// //Create Game
-// function createGame() {
-    // hangmanImage.src = "images/hangman/hangman.png";                            //setup
-    // let token = getRandomInt(numberOfWords);
-    // word = words[token]["word"].toUpperCase();
-    // wordLength = word.length;
-    // hint = words[token]["hint"];
-    // guessDisplay.innerHTML = `<p><strong>Guesses: </strong>0/7</p>`;
-    // buttonsDisplay.innerHTML = "";
-    // correctGuesses = 0;
-    // incorrectGuesses = 0;
-
-//     for (let i = 1; i <= 26; i++) {
-//         addButton(String.fromCharCode(64 + i));
-//     }
-
-//     //Call function to display spaces for word
-//     createBlanks();
-//     //Call function to display hint for word
-//     createHint();
-// }
