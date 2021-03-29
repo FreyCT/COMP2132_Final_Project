24b448b306c2391eef41fe983d8d5672e053b782
cantseemyeyes <jackyyet@usc.edu>
2021-03-28 20:03:04 -0700

    muuttthhhaaa fuuuuckkaaaa

--- scripts/dicegame.js
+++ scripts/dicegame.js
@@ -1,12 +1,21 @@
 
 
-
+let btnRestart = document.getElementById('btn-restart')
+btnRestart.addEventListener('click', function(){
+    if(window.confirm("Are you sure you want to restart the game?")){
+        location.reload();
+    }
+})
 
 const player = document.getElementsByClassName('dice-image')[0]
 player.style.opacity = 0
 
 const computer = document.getElementsByClassName('dice-image')[1]
 computer.style.opacity = 0
+
+let PlayerDiceResult = document.getElementsByClassName('dice-result')[0]
+let ComputerDiceResult = document.getElementsByClassName('dice-result')[1]
+
 
 
 
@@ -19,6 +28,7 @@ let rollNumber = 0;
 let roundNumber = 0
 let roundTallyPlayer = []
 let roundTallyComputer = []
+
 maxImageNumber = 6;
 
 const interval = 100;
@@ -40,6 +50,7 @@ rollDice.addEventListener('click', function(){
         clearInterval(imagehandler);    //original main intention
         let playerRandomNumber = Math.floor(Math.random() * 6)+1
         player.src = `images/dice/dice-${playerRandomNumber}.png`;
+        PlayerDiceResult.innerHTML = `Dice result: ${playerRandomNumber}`
         $(".roll").eq(rollNumber).text(playerRandomNumber)
         computerRoll()
 
@@ -47,14 +58,14 @@ rollDice.addEventListener('click', function(){
         if(roundTallyPlayer.length == 2){
             roundTallyPlayer = []
             roundTallyPlayer.push(playerRandomNumber)
-            console.log( roundTallyPlayer )
         }else{
             roundTallyPlayer.push(playerRandomNumber)
-            console.log( roundTallyPlayer )
         }
         if(roundTallyPlayer.length == 2){
             let result = 0
-            if(roundTallyPlayer[0] == roundTallyPlayer[1]){
+            if((roundTallyPlayer[0] == 1 || roundTallyPlayer[1]) == 1){
+                playerResult = 0
+            }else if(roundTallyPlayer[0] == roundTallyPlayer[1]){
                 result = (roundTallyPlayer[0] + roundTallyPlayer[1])*2
             }else{
                 result = roundTallyPlayer[0] + roundTallyPlayer[1]
@@ -81,20 +92,21 @@ function computerRoll(){
         clearInterval(imagehandler);
         let computerRandomNumber = Math.floor(Math.random() * 6)+1
         computer.src = `images/dice/dice-${computerRandomNumber}.png`;
+        ComputerDiceResult.innerHTML = `Dice result: ${computerRandomNumber}`
         $(`.roll`).eq(rollNumber).text(computerRandomNumber)
         rollDice.style.display = 'block'
 
         if(roundTallyComputer.length == 2){
             roundTallyComputer = []
             roundTallyComputer.push(computerRandomNumber)
-            console.log( roundTallyComputer )
         }else{
             roundTallyComputer.push(computerRandomNumber)
-            console.log( roundTallyComputer )
         }
         if(roundTallyComputer.length == 2){
             let computerResult = 0
-            if(roundTallyComputer[0] == roundTallyComputer[1]){
+            if((roundTallyComputer[0] == 1 || roundTallyComputer[1]) == 1){
+                computerResult = 0
+            }else if(roundTallyComputer[0] == roundTallyComputer[1]){
                 computerResult = (roundTallyComputer[0] + roundTallyComputer[1])*2
             }else{
                 computerResult = roundTallyComputer[0] + roundTallyComputer[1]
@@ -102,29 +114,53 @@ function computerRoll(){
             $(".points").eq(roundNumber + 1).text(computerResult)
             roundNumber += 2
         }
+        checkPoints()
     }, 500);
+
+    
 
 };
 
 
 
-numberOfPoints = document.getElementsByClassName("points")
+let numberOfPoints = document.getElementsByClassName("points")
 let totalPlayerScore = document.getElementById("totalPlayerScore")
 let totalComputerScore = document.getElementById("totalComputerScore")
 
+function checkPoints(){
+    let emptyPoints = numberOfPoints.length
+    for (i = 0; i<numberOfPoints.length; i++){
+        if("points" != numberOfPoints[i].innerHTML){
+            emptyPoints -= 1
+        }
+    }
 
-
-emptyPoints = 0
-for (i = 0; i<numberOfPoints.length; i++){
-    if("points" == numberOfPoints[i].innerHTML){
-        console.log(numberOfPoints[i].innerHTML)
-        emptyPoints ++
+    if(emptyPoints == 0){
+        totalPlayerScore.innerHTML = parseInt($(".points").eq(0).text()) + parseInt($(".points").eq(2).text()) + parseInt($(".points").eq(4).text())
+        totalComputerScore.innerHTML = parseInt($(".points").eq(1).text()) + parseInt($(".points").eq(3).text()) + parseInt($(".points").eq(5).text())
+        rollDice.style.display = 'none'
+        announceWinner()
     }
 }
 
 
-if(emptyPoints == 0){
-    totalPlayerScore.innerHTML = "hello"
-    totalComputerScore.innerHTML = "hello"
-}
+function announceWinner(){
+    setTimeout(() => {
+        if(totalPlayerScore.innerHTML > totalComputerScore.innerHTML){
+            if(window.confirm("You won, would you like to play again?")){
+                location.reload();
+            }
+        }
+        if(totalPlayerScore.innerHTML < totalComputerScore.innerHTML){
+            if(window.confirm("You lost, would you like to play again?")){
+                location.reload();
+            }
+        }
+        if(totalPlayerScore.innerHTML == totalComputerScore.innerHTML){
+            if(window.confirm("You tied, would you like to play again?")){
+                location.reload();
+            }
+        }
 
+    }, 1500)
+}
